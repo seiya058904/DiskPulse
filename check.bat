a6b887d3ac7cf5b5381e84b0a1a4cd5226726d97
seiya058904 <seiya058904@users.noreply.github.com>
2026-05-24 16:35:44 +0800

    标题改为 DiskPulse

--- check.bat
+++ check.bat
@@ -167,7 +167,7 @@ $html = @'
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<title>Storage Monitor</title>
+<title>DiskPulse</title>
 <style>
   *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
 
